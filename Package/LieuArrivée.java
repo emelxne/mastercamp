fa61309ce4705f4f5d3f3337faee6ee9f86478c1
Emeline <70700006+emelxne@users.noreply.github.com>
2021-06-08 10:49:18 +0200

    Delete Package directory

--- Package/LieuArrivée.java
+++
@@ -1,9 +0,0 @@
-package Package;
-
-public enum LieuArrivée {
-	;
-
-	private int urgences;
-	private int accueil;
-
-}
--- Package/hSoin.java
+++
@@ -1,5 +0,0 @@
-package Package;
-
-public class hSoin {
-	heureAdministration : String
-}
